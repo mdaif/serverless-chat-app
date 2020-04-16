bcf8d6d57a24cbfbc46a55306c3e1611c9a24d9f
Mohamed Daif <daif.control@gmail.com>
2020-04-16 19:50:41 +0200

    feat: add client

---
+++ serverless-chat-app/src/websocket/broadcast.ts
@@ -0,0 +1,64 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const connectionsTable = process.env.CONNECTIONS_TABLE
+const stage = process.env.STAGE
+const apiId = process.env.API_ID
+const region = process.env.REGION
+const connectionParams = {
+    apiVersion: "2018-11-29",
+    endpoint: `${apiId}.execute-api.${region}.amazonaws.com/${stage}`
+}
+
+const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Websocket connect', event)
+
+    const senderConnectionId = event.requestContext.connectionId
+    const message = event.body
+    console.log('Getting all connections for connection: ', senderConnectionId)
+
+    const connections = await docClient.scan({
+        TableName: connectionsTable
+    }).promise()
+
+    for (const connection of connections.Items) {
+        const receiverConnectionId = connection.id
+        await sendMessageToClient(senderConnectionId, message, receiverConnectionId)
+    }
+    return {
+        statusCode: 200,
+        body: ''
+    }
+}
+
+async function sendMessageToClient(
+    senderConnectionId: string, message: string, receiverConnectionId: string) {
+    try {
+        console.log('Sending message to a connection', receiverConnectionId)
+        const payload = {
+            'message': `${senderConnectionId} says: ${message}`
+        }
+        await apiGateway.postToConnection({
+            ConnectionId: receiverConnectionId,
+            Data: JSON.stringify(payload),
+        }).promise()
+    } catch (e) {
+        console.log('Failed to send message', JSON.stringify(e))
+        if (e.statusCode === 410) {
+            console.log('Stale connection')
+
+            await docClient.delete({
+                TableName: connectionsTable,
+                Key: {
+                    id: receiverConnectionId
+                }
+            }).promise()
+
+        }
+    }
+}
