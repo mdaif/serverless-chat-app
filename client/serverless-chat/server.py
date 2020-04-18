c04e1345c99f06830aa1e2311ebff0fb22e0da91
Mohamed Daif <daif.control@gmail.com>
2020-04-18 17:32:52 +0200

    feat: add id_token to displayed info

--- client/serverless-chat/server.py
+++ client/serverless-chat/server.py
@@ -76,9 +76,7 @@ def callback_handling(*args, **kwargs):
     auth0.authorize_access_token()
     resp = auth0.get('userinfo')
     userinfo = resp.json()
-    print("#" * 20 + "ID TOKEN" + "#" * 20)
-    print(auth0.token['id_token'])
-    print("#" * 48)
+    session[constants.ID_TOKEN] = auth0.token['id_token']
     session[constants.JWT_PAYLOAD] = userinfo
     session[constants.PROFILE_KEY] = {
         'user_id': userinfo['sub'],
@@ -103,9 +101,11 @@ def logout():
 @app.route('/dashboard')
 @requires_auth
 def dashboard():
+    data = session[constants.JWT_PAYLOAD]
+    data['id_token'] = session[constants.ID_TOKEN]
     return render_template('dashboard.html',
                            userinfo=session[constants.PROFILE_KEY],
-                           userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4))
+                           userinfo_pretty=json.dumps(data, indent=4))
 
 
 if __name__ == "__main__":
